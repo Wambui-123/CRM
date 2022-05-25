42b9a5627e8fa79de5f80147718ca524f86a8247
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-05-25 14:07:37 +0300

    used class based views

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from leads.views import landing_page, LandingPageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPageView.as_view(), name="landing_page"),
+    # path('', landing_page, name="landing_page"),
     path('leads/', include('leads.urls', namespace="leads"))
 ]
