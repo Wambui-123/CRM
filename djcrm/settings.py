8060b9bac0df50dadc3916c3783d8d55d69cf7e1
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-06-29 22:33:16 +0300

    deployment

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -177,7 +177,7 @@ if not DEBUG:
     SECURE_HSTS_PRELOAD = True
     X_FRAME_OPTIONS = "DENY"
 
-    ALLOWED_HOSTS = ["*"]
+    # ALLOWED_HOSTS = ["*"]
 
     # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     # EMAIL_HOST = env("EMAIL_HOST")
